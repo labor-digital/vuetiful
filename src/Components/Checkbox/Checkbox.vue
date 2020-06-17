39e13bc13ebac582e4f9bd70da7215c9d919315c
Martin Neundorfer <m.neundorfer@labor.digital>
2020-06-17 21:51:03 +0200

    fix(Checkbox): fixe multiple reactivity issues

--- src/Components/Checkbox/Checkbox.vue
+++ src/Components/Checkbox/Checkbox.vue
@@ -18,17 +18,19 @@
 
 <template>
 	<div class="checkbox" v-if="hasItems">
-		<label v-for="(item, index) in prepareItems" v-show="item.show" class="checkbox__label" :class="intClasses(item)" :ref="'checkbox-'+index">
+		<label v-for="(item, index) in prepareItems" v-show="item.show"
+               class="checkbox__label"
+               :class="intClasses(item)"
+               :ref="'checkbox-'+index">
 			<span v-if="labelSide === 'left'" class="checkbox__labelText checkbox__labelText--left">{{item.label}}</span>
-			<input v-show="hasCustomCheckIcon" class="checkbox__input" :ref="'checkboxInput-'+index"
-				   type="checkbox"
-				   :value="item.value"
-				   :checked="item.checked"
+			<input v-show="hasCustomCheckIcon" class="checkbox__input"
+                   type="checkbox"
 				   :required="item.required"
 				   :disabled="item.disabled"
-				   @input="updateValue(index, $event)"
+                   v-model="item.checked"
+				   @input="updateValue(item)"
 			/>
-			<!-- @slot Add your custom check icon if needed.  -->
+			<!-- @slot Add your custom check icon if needed. -->
 			<slot name="customCheckIcon"></slot>
 			<span v-if="labelSide === 'right'" class="checkbox__labelText checkbox__labelText--right">{{item.label}}</span>
 		</label>
@@ -41,6 +43,8 @@
 
 <script lang="ts">
 	import {forEach} from "@labor-digital/helferlein/lib/Lists/forEach";
+    import {map} from '@labor-digital/helferlein/lib/Lists/map';
+    import {isArray} from '@labor-digital/helferlein/lib/Types/isArray';
 	import {isEmpty} from "@labor-digital/helferlein/lib/Types/isEmpty";
 	import {isUndefined} from "@labor-digital/helferlein/lib/Types/isUndefined";
 	import {CheckboxInputs} from "./Checkbox.interfaces";
@@ -68,27 +72,28 @@
 			 */
 			error: {
 				type: String
-			}
-		},
-		data() {
-			return {
-				value: []
-			};
+			},
+            
+            value: {
+			    type: Array,
+                default: []
+            }
 		},
 		computed: {
 			prepareItems() {
+			    let selectedItems = isEmpty(this.value) ? [] : map(this.value, (e) => e.value);
 				let preparedItems = [];
 				forEach(this.items, (item, index) => {
-					let preparedItem = {
-						id: index,
-						value: isUndefined(item.value) ? item : item.value,
-						label: isUndefined(item.label) ? item : item.label,
-						checked: isUndefined(item.checked) ? false : item.checked,
-						required: isUndefined(item.required) ? false : item.required,
-						disabled: isUndefined(item.disabled) ? false : item.disabled,
-						show: isUndefined(item.show) ? true : item.show
-					};
-					preparedItems.push(preparedItem);
+					preparedItems.push({
+                        value: isUndefined(item.value) ? item : item.value,
+                        label: isUndefined(item.label) ? item : item.label,
+                        checked: isUndefined(item.checked) ?
+                            selectedItems.indexOf(item.value) !== -1 :
+                            item.checked,
+                        required: isUndefined(item.required) ? false : item.required,
+                        disabled: isUndefined(item.disabled) ? false : item.disabled,
+                        show: isUndefined(item.show) ? true : item.show
+                    });
 				});
 				return preparedItems;
 			},
@@ -104,42 +109,20 @@
 				return !isEmpty(this.items) || !isUndefined(this.items);
 			},
 			intClasses(item) {
-				let classes = "";
-				item.required ? classes += "checkbox__label--required " : classes.replace("checkbox__label--required", "");
-				item.disabled ? classes += "checkbox__label--disabled " : classes.replace("checkbox__label--disabled", "");
-				return classes;
+			    return {
+                    "checkbox__label--required": item.required,
+                    "checkbox__label--disabled": item.disabled,
+                    "checkbox__label--checked": item.checked
+                }
 			},
-			updateValue(index, e?) {
-				let element = {
-					id: index,
-					value: e.target.value,
-					label: e.target.labels[0].innerText,
-					checked: e.target.checked,
-					required: e.target.required,
-					disabled: e.target.disabled,
-					element: e.target
-				};
-				if (element.checked) this.value.push(element);
-				else this.value = this.value.filter(item => item.id !== element.id);
-				
+			updateValue(item) {
+			    const isChecked = !item.checked;
+				const valueClone = isArray(this.value) ? [...this.value]: [];
+				if (isChecked) valueClone.push(item);
+				else valueClone = valueClone.filter(val => val.id !== item.id);
+                this.$emit("input", valueClone);
 			}
-		},
-		watch: {
-			value: function () {
-				forEach(this.items, (item, index) => {
-					this.$refs["checkbox-" + index][0].classList.remove("checkbox__label--checked");
-					this.$refs["checkboxInput-" + index][0].checked = false;
-				});
-				
-				forEach(this.value, (value, index) => {
-					this.$refs["checkbox-" + value.id][0].classList.add("checkbox__label--checked");
-					this.$refs["checkboxInput-" + value.id][0].checked = true;
-					
-				});
-				this.$emit("input", this.value);
-			}
-		}
-		
+		}		
 	};
 </script>
 
