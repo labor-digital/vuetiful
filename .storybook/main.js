17ff0f173db2c40133571e80ad74930b9fb2b04a
Martin Neundorfer <m.neundorfer@labor.digital>
2020-10-30 18:37:24 +0100

    feat: update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,18 @@
 module.exports = {
-	stories: ["../stories/**/*.stories.js"],
-	addons: [
-		{
-			name: "@labor-digital/asset-building/dist/Interop/Storybook/registration",
-			options: {
-				app: {
-					extensions: ["@labor-digital/asset-building-env-vuejs"]
-				}
-			}
-		},
-		"@storybook/addon-viewport/register",
-		"@storybook/addon-knobs/register",
-		"@storybook/addon-actions",
-		"@storybook/addon-links",
-		"@storybook/addon-docs"
-	]
+    stories: ['../stories/**/*.stories.js'],
+    addons: [
+        {
+            name: '@labor-digital/asset-building/interop/storybook',
+            options: {
+                app: {
+                    extensions: ['@labor-digital/asset-building-env-vuejs']
+                }
+            }
+        },
+        '@storybook/addon-viewport/register',
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-actions',
+        '@storybook/addon-links',
+        '@storybook/addon-docs'
+    ]
 };
