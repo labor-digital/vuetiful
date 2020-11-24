503595cafc0f5027f8df326dcfe296aa2b38cce0
Martin Neundorfer <m.neundorfer@labor.digital>
2020-11-24 23:50:36 +0100

    fix(viewFactory): reimplement previous prop resolution

--- src/Utils/viewFactory.ts
+++ src/Utils/viewFactory.ts
@@ -16,8 +16,44 @@
  * Last modified: 2020.11.19 at 12:05
  */
 
-import {isPlainObject, map, PlainObject} from '@labor-digital/helferlein';
+import {forEach, isEmpty, isPlainObject, map, PlainObject} from '@labor-digital/helferlein';
 import {mergeData} from 'vue-functional-data-merge';
+
+/**
+ * Merges the props given to the view component into the props of the
+ * implementation object. However, empty props of the view component will be ignored
+ * and filled with the props of the parent
+ *
+ * @param parentProps
+ * @param localProps
+ */
+function mergeProps(parentProps: PlainObject, localProps: PlainObject)
+{
+    const props = {...parentProps};
+    forEach(localProps, (v, k) => {
+        if (!isEmpty(v) || isEmpty(parentProps[k])) {
+            props[k] = v;
+        }
+    });
+    return props;
+}
+
+/**
+ * Helper to inherit the template props but without any required properties ->
+ * If there is a missing property the real template component will still throw an error
+ * but we, as a proxy should not
+ * @param template
+ */
+function makeLocalProps(template)
+{
+    return map(isPlainObject(template.props) ? template.props : {}, v => {
+        if (isPlainObject(v) && v.required) {
+            v = {...v};
+            v.required = false;
+        }
+        return v;
+    });
+}
 
 /**
  * Used to provide the View component definition on an abstract.
@@ -30,14 +66,14 @@ export default function (template, alternativeParent?: PlainObject) {
     return {
         functional: true,
         inheritAttrs: false,
-        props: map(isPlainObject(template.props) ? template.props : {}, () => null),
+        props: makeLocalProps(template),
         render(h, ctx)
         {
             const parent = alternativeParent ?? ctx.parent;
             const localData = {
                 ref: 'view',
                 on: parent.$listeners,
-                props: parent.$props,
+                props: mergeProps(parent.$props, ctx.props),
                 scopedSlots: parent.$scopedSlots
             };
             return h(template, mergeData(ctx.data, localData), [parent.children ?? ctx.children]);
